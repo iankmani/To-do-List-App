cfa1922fdac9befb4728999c05e7b6528f496031
Ian Kimani <ian.kiarie2002@gmail.com>
2024-09-18 13:43:30 +0300

    change color of G

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -50,7 +50,7 @@ const handleDeleteTask = (index) => {
   return (
     <div className='TodoList-app'>
         <div className='TodoList-input'>
-        <h1>G-PANGE</h1>
+        <h1><span className='g'>G</span>-PANGE</h1>
         <input type="text" value={newTask} onChange={handleInputChange} placeholder="Enter task"/>
         <button className='add-button' onClick={handleAddTask}>Add Task</button>
         </div>
